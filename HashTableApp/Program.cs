5584201090bc5506376c8da07d2f632ada07f974
Munsi Abdul Al Mamun <mdabdulalmamoon@gmail.com>
2022-01-20 14:34:38 +0800

    Add project files.

---
+++ HashTableApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable app = new Hashtable();
+
+            app.Add("1", "Hello World");
+            app.Add("2", "Hello My World");
+
+            foreach (DictionaryEntry item in app)
+            {
+                Console.WriteLine(item.Key + " "+item.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
